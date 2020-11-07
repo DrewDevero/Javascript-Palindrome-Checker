f37b88e41210dfa0a9382cde6969553b5d048667
Drew Devero <deverobelfon@gmail.com>
2020-11-07 14:59:39 -0500

    added a custom try catch error message and an else case for empty submissions

--- scripts/palindromeFunctions.js
+++ scripts/palindromeFunctions.js
@@ -11,7 +11,10 @@ let palinTests = [
     [0,3,4,4,3,0],
     7778889888777,
     54546546666677,
-    "049355"
+    "049355",
+    "",
+    [],
+    // {}   // uncomment for custom error to throw when this submission is reached
 ];
 let i = 0;
 
@@ -19,7 +22,14 @@ let i = 0;
 
 function palin (possiblePalin) {    
     'use strict';
-    if (Array.isArray(possiblePalin)) {
+    try {
+        if (possiblePalin == {}) throw `This is a/an ${possiblePalin}. Please try again with a filled array, string, or a number`;
+    }
+    catch(err) {
+        showResult.innerHTML = err;
+        body.backgroundColor = "yellow";
+    }
+    if (Array.isArray(possiblePalin) && possiblePalin.length !== 0) {
         let word = possiblePalin;
         let wordHalfLength = Math.floor(word.length / 2);
         let firstHalf = word.slice(0, wordHalfLength);
@@ -27,7 +37,7 @@ function palin (possiblePalin) {
         let reverseSecondHalf = secondHalf.reverse();
         console.log(wordHalfLength); // logs out stages of the operation, so. . .
         console.log(firstHalf); // . . .these line are not. . .
-        console.log(reverseSecondHalf); // . . .necessary for the end result.
+        console.log(reverseSecondHalf); // . . .necessary for the end result
             if (firstHalf.join() == reverseSecondHalf.join()) {
                 console.log(`${word} IS a palindrome.`);
                 showResult.innerHTML = `${word} IS a palindrome.`;
@@ -43,8 +53,8 @@ function palin (possiblePalin) {
                     showResult.innerHTML = `${word} is NOT a palindrome.`;
                     body.backgroundColor = "red";                    
                     }
-            }
-    } else if (!Array.isArray(possiblePalin) && typeof possiblePalin !== "number") {
+            }            
+    } else if (!Array.isArray(possiblePalin) && typeof possiblePalin !== "number" && possiblePalin.length !== 0) {
         possiblePalin.trim();
         let word = Array.from(possiblePalin);
         let wordHalfLength = Math.floor(word.length / 2);
@@ -53,7 +63,7 @@ function palin (possiblePalin) {
         let reverseSecondHalf = secondHalf.reverse();  
             console.log(wordHalfLength); // logs out stages of the operation, so. . .
         console.log(firstHalf); // . . .these line are not. . .
-        console.log(reverseSecondHalf); // . . .necessary for the end result.
+        console.log(reverseSecondHalf); // . . .necessary for the end result
             if (firstHalf.join() == reverseSecondHalf.join()) {
                 console.log(`${word} IS a palindrome.`);
                 showResult.innerHTML = `${word} IS a palindrome.`;  
@@ -69,8 +79,8 @@ function palin (possiblePalin) {
                     showResult.innerHTML = `${word} is NOT a palindrome.`;
                     body.backgroundColor = "red";                   
                     }
-            }
-    } else if (typeof possiblePalin === "number") {
+            }           
+    } else if (typeof possiblePalin === "number" && possiblePalin.length !== 0) {
         let toString = possiblePalin.toString()
         let word = Array.from(toString);
         let wordHalfLength = Math.floor(word.length / 2);
@@ -79,7 +89,7 @@ function palin (possiblePalin) {
         let reverseSecondHalf = secondHalf.reverse();  
             console.log(wordHalfLength); // logs out stages of the operation, so. . .
         console.log(firstHalf); // . . .these line are not. . .
-        console.log(reverseSecondHalf); // . . .necessary for the end result.
+        console.log(reverseSecondHalf); // . . .necessary for the end result
             if (firstHalf.join() == reverseSecondHalf.join()) {
                 console.log(`${word} IS a palindrome.`);
                 showResult.innerHTML = `${word} IS a palindrome.`;  
@@ -95,19 +105,20 @@ function palin (possiblePalin) {
                     showResult.innerHTML = `${word} is NOT a palindrome.`;
                     body.backgroundColor = "red";
                     }
-            }
+            }    
     } else {
-        throw "Please try again with an array, string, or numbers.";
+        showResult.innerHTML = `This submission is EMPTY. Please try again with a filled array, string, or a number`;
+        body.backgroundColor = "yellow";
     }
 }
 
 // add click event where every time the button is clicked:
 // - an item from the array palinTests is processed through function palin()
 // - the next item in the array will be processed the next time the button is clicked
-// - the input position on the list resets to the beginning of the list once all items on the list have been processed once.
+// - the input position on the list resets to the beginning of the list once all items on the list have been processed once
 
 function palinTestIter() {
-    if (i < palinTests.length) {
+if (i < palinTests.length) {
         palin(palinTests[i]);
         return i++;
     } else {
